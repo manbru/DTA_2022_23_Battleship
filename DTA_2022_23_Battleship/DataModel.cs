b96ce1f7d73f8e39ebbed707d0c1e3d86963d99f
manbru <man.bru@hotmail.com>
2023-04-17 20:01:31 +0200

    EF and data classes added

---
+++ DTA_2022_23_Battleship/DataModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+
+namespace DTA_2022_23_Battleship {
+    public class BloggingContext : DbContext {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Match> Matches { get; set; }
+
+        public string DbPath { get; }
+
+        public BloggingContext() {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
+
---
+++ DTA_2022_23_Battleship/Match.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTA_2022_23_Battleship {
+    public class Match {
+        public int MatchId { get; set; }
+        public int EndTime { get; set; }
+        public User Winner { get; set; }
+        public User Loser { get; set; }
+        public int Turns { get; set; }
+    }
+}
